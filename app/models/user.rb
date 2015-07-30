d45f4bd15f5bfd85afa06a332cb2da4e3bb6f742
Ezra Raez <ezRAez10@gmail.com>
2015-07-29 19:35:28 -0700

    buttons working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
+  validates :username, presence: true, uniqueness: true, length: { in: 3..15 }
   has_many :comics
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
   get 'loan_check/:comic_id', to: 'comics#loan_check'
   get 'checkin_check/:comic_id', to: 'comics#checkin_check'
   get 'checkin/:comic_id', to: 'comics#checkin'
